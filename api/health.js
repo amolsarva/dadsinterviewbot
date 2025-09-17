fc17cd08bddf01fda092e25ff686cafd4a21a9a8
Amol Sarva <amol@Amols-Aikito-Mac.local>
2025-09-17 13:34:38 -0400

    Remove legacy Node API handlers

--- api/health.js
+++
@@ -1,5 +0,0 @@
-export const config = { runtime:'nodejs' }
-export default async function handler(req,res){
-  const env={ GOOGLE:!!process.env.GOOGLE_API_KEY, BLOB:!!process.env.BLOB_READ_WRITE_TOKEN, SENDGRID:!!process.env.SENDGRID_API_KEY, MAIL_FROM:process.env.MAIL_FROM||null }
-  return res.status(200).json({ ok:true, env })
-}
